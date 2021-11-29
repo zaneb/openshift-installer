1c5102b6b9bce5fd1f08255dfcc71ceb29ebf25e
Zane Bitter <zbitter@redhat.com>
2021-11-29 15:24:38 -0500

    Reference openshift/cluster-api-provider-baremetal directly
    This started as a fork of a metal3-io repo, but has long since diverged,
    and the actual go.mod file declares the module name to be openshift/ not
    metal3-io/.

--- pkg/asset/machines/worker.go
+++ pkg/asset/machines/worker.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 
 	"github.com/ghodss/yaml"
-	baremetalapi "github.com/metal3-io/cluster-api-provider-baremetal/pkg/apis"
-	baremetalprovider "github.com/metal3-io/cluster-api-provider-baremetal/pkg/apis/baremetal/v1alpha1"
+	baremetalapi "github.com/openshift/cluster-api-provider-baremetal/pkg/apis"
+	baremetalprovider "github.com/openshift/cluster-api-provider-baremetal/pkg/apis/baremetal/v1alpha1"
 	machineapi "github.com/openshift/api/machine/v1beta1"
 	alibabacloudapi "github.com/openshift/cluster-api-provider-alibaba/pkg/apis"
 	alibabacloudprovider "github.com/openshift/cluster-api-provider-alibaba/pkg/apis/alibabacloudprovider/v1beta1"
